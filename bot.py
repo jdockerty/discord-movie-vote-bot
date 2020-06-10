cb934c5c81af09ebf4670b5c3bef61e5e4d05bb6
Jack Dockerty <jdockerty19@gmail.com>
2020-06-10 14:05:24 +0100

    Added startswith prefix to avoid recursive bot calling itself with instructions.

--- bot.py
+++ bot.py
@@ -8,7 +8,6 @@ import json
 
 dotenv.load_dotenv()
 
-
 class MyClient(discord.Client):
 
     votes = {}
@@ -19,22 +18,24 @@ class MyClient(discord.Client):
         print('Logged on as {0}!'.format(self.user))
 
     async def on_message(self, message):
-        print(message)
-        print(message.channel.id)
         if "!newvote" in message.content:
-            # print(type(message.content))
             await self.new_vote(message)
 
-        elif "!vote" in message.content:
+        elif message.content.startswith("!vote"):
             await self.add_vote(message)
 
-        elif "!changevote" in message.content:
+        elif message.content.startswith("!changevote"):
             await self.change_vote(message)
 
         elif "!endvote" in message.content:
+
             # Finds the largest item in the dictionary by sorting based on the vote count value.
-            winner_key = max(self.votes, key= (lambda key_val: self.votes[key_val]["Vote Count"]) )
+            winner_key = max(self.votes, key= (lambda key_val: self.votes[key_val]["Vote Count"]))
             print(self.votes[winner_key]["Movie Name"])
+            chan = self.get_channel(int(os.getenv("CHANNEL_ID")))
+            await chan.send(f"{self.votes[winner_key]['Movie Name']} wins with {self.votes[winner_key]['Vote Count']} votes.")
+            self.already_voted.clear()
+            self.votes.clear()
 
     def get_message_content(self, message_string):
         return message_string.content.split(" ")[1:]
@@ -45,15 +46,15 @@ class MyClient(discord.Client):
             movies = message.content.replace("!newvote ", "").split(", ")
             for i, movie in enumerate(movies, start=1):
                 self.votes[i] = {"Movie Name" : movie, "Vote Count" : 0}
+
             movie_string = ""
             i = 1
             for movie in self.votes.values():
-                print(movie)
                 movie_string += f"{i}: {movie['Movie Name']}\n"
                 i += 1
             chan = self.get_channel(int(os.getenv("CHANNEL_ID")))
-            await chan.send(f"Movies to vote on: \n {movie_string}")
-            
+            await chan.send(f"Use `!vote X Y Z` to place your votes\nChanges to votes are done by using `!changevote X Y Z`\n Movies to vote on: \n {movie_string}")
+
         else:
             chan = self.get_channel(int(os.getenv("CHANNEL_ID")))
             await chan.send("Only those with the role of 'Admin' can start votes.")
@@ -82,16 +83,13 @@ class MyClient(discord.Client):
             print(self.already_voted)
         
         
-        
-
 
     async def change_vote(self, new_options):
         message_author = new_options.author.name
         old_choices = None
         new_choices = self.get_message_content(new_options)
-        # print(self.already_voted)
         for voter in self.already_voted.keys():
-            print("VOTER", voter)
+   
             if voter == message_author:
                 print(voter, message_author)
                 old_choices = self.already_voted[message_author]
@@ -105,6 +103,7 @@ class MyClient(discord.Client):
         for choice in new_choices:
             self.votes[int(choice)]["Vote Count"] += j
             j -= 1
+
         chan = self.get_channel(int(os.getenv("CHANNEL_ID")))
         await chan.send(message_author + " vote changed.")
         print("Vote changed: ", message_author)
